6fdce9b469b65e41124614149b92d318c254fc5c
Nemanja Manot <manotnemanja@gmail.com>
2020-10-09 10:01:51 +0200

    Updates

---
+++ precache-manifest.a800f102c9c08f7245389553511a8229.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c656227282b5602db19d8ae2b89421c8",
+    "url": "/kanban-board/index.html"
+  },
+  {
+    "revision": "84cb93e32b6e76621991",
+    "url": "/kanban-board/static/css/main.c5e775c1.chunk.css"
+  },
+  {
+    "revision": "773e9d2f352fb1711caa",
+    "url": "/kanban-board/static/js/2.29ed4444.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kanban-board/static/js/2.29ed4444.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84cb93e32b6e76621991",
+    "url": "/kanban-board/static/js/main.fae94d44.chunk.js"
+  },
+  {
+    "revision": "ba2096fa3b770a19f0f1",
+    "url": "/kanban-board/static/js/runtime-main.8e469a6a.js"
+  }
+]);
